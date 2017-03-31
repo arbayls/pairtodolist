e7e98e4a2a505c9413f9bd693864c37e7445698c
ellebayless <elle@ellebayles.com>
2017-03-31 13:01:52 -0700

    init commit

---
+++ public/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+//Grab data from database
+$.getJSON('http://localhost:8000/todos', function(data){
+  // var items = [];
+  // $.each(data,function(key,val){
+  //   items.push(`<li> val </li>`);
+    console.log("Hello");
+
+  // $('#items').append()
+  // }
+
+
+
+});
+
+
+
+
+
+//show items in database on html page
+
+
+
+
+
+
+}); //End of Jquery
